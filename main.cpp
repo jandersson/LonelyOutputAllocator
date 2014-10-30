3ad7b1671e48832819a27e7669cde59d50aadb4b
Jonas Andersson <jonas123@gmail.com>
2014-10-29 22:40:13 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ECE-427/527
+Integrated Interconnection Networks
+Project 2
+Write a program to implement the Lonely Output Allocator.
+The input to the program is (in the following exact order and format):
+(1) The number of rows (input ports) and the number of columns (output ports)
+of the Request Matrix
+(2) The Request matrix with entries of 1 (indicating request) or 0 (indicated no request)
+separated by a space
+
+The output of the program should be (a) the Count Matrix and (b) the Grant Matrix under (b1)
+the Input-First and (b2) the Output-First criterion.
+
+Include Comments and produce output for 3 different Request matrices of your choice
+
+Sample Input:
+-------------
+14 16
+1 0 0 0 0 0 0 0 0 0 0 0 0...
+0 1 ...
+.
+.
+.
+
+*/
+
+int main() {
+}
