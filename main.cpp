584e058b53f8166e3c120792d906c5c40c14e7b5
Jonas Andersson <jonas@andersson.io>
2014-11-10 20:23:08 -0600

    Parse input file and retrieve agents and ports

--- main.cpp
+++ main.cpp
@@ -22,30 +22,53 @@ Sample Input:
 .
 .
 .
-
 */
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <stdlib.h>
+#include <vector>
+#include <sstream>
+
 
 using namespace std;
 
 void openDataFile(){
+    /* Opens a data file for reading */
     string fileName = "mytest.txt";
+    char delimiter = ' ';
+    string token;
+    stringstream iss;
     ifstream dataFile;
+    vector<int> requestMatrix;
     string line = "";
     dataFile.open(fileName);
     int agents = 0;
     int ports = 0;
+    cout << dataFile.is_open() << endl;
     if (dataFile.is_open()){
-        while (getline(dataFile, line, ' ')){
-            int agents = atoi(line.c_str());
-            int ports = atoi(line.c_str());
+        getline(dataFile, line, ' ');
+        cout << line << endl;
+        agents = atoi(line.c_str());
+        getline(dataFile, line);
+        ports = atoi(line.c_str());
+        //Get the entire line, then parse the line for spaces (whatever delimiter)
+        while(getline(dataFile, line, '\n')){
+            iss << line;
+            while(getline(iss, token, delimiter)){
+                cout << token;
+                requestMatrix.push_back(atoi(token.c_str()));
+            }
+            iss.clear();
+            cout << '\n';
         }
+
+            }
+
+    cout << "Agents: " << agents << endl;
+    cout << "Ports: " << ports << endl;
+    for(auto it = requestMatrix.begin(); it != requestMatrix.end(); it++){
+        cout << *it;
     }
-    cout << agents << endl;
-    cout << ports << endl;
 }
 
 int main() {
